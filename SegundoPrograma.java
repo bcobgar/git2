c396b49ae0df86ac0b3a3f12bf6435f1c590c59f
bcobgar@g.educaand.es <bcobgar@g.educaand.es>
2024-10-25 22:45:00 +0200

    creo segundo porgrama

---
+++ SegundoPrograma.java
@@ -0,0 +1,7 @@
+public class SegundoPrograma {
+    public static void main(String[] args) {
+		    int num1 = 5, num2 = 3;
+        System.out.println("este es el segundo programa");
+        
+    }
+}
